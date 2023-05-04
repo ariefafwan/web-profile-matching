9e66ccfdd6540d655ae9667d992e257d2289f3fa
Afwan <tmariefafwan@gmail.com>
2023-05-04 23:58:11 +0700

    ganti ke laravel-mix

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -7,7 +7,11 @@
 @stop
 
 @section('css')
-        {{-- @vite(['resources/js/app.js']) --}}
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+@stop
+
+@section('js')
+        <script src="{{ mix('js/app.js') }}" defer></script>
 @stop
